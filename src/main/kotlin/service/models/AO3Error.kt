1d44d30684ae1ccf9e19e0443e0f7dcb433a91bf
Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2021-07-21 01:38:22 +0800

    Add an error type and more documentation for errors

--- src/main/kotlin/service/models/AO3Error.kt
+++ src/main/kotlin/service/models/AO3Error.kt
@@ -1,6 +1,22 @@
 package service.models
 
 sealed class AO3Error: Throwable() {
-    data class RedirectError(val redirectUrl: String): AO3Error()
+
+    /**
+     * Represents a scenario where AO3 returns a response prompting for a redirect (e.g. 302, 304).
+     */
+    sealed class RedirectError(open val redirectUrl: String): AO3Error()
+        /**
+         * Represents a scenario where AO3 is redirecting the client to the works of another tag, in response to
+         * a request for the works of a particular tag. This indicates that these two tags are synonyms, and the
+         * destination tag is the canonical name for the original requested tag.
+         */
+        data class TagSynonymError(override val redirectUrl: String) : RedirectError(redirectUrl) {
+            val synonymTagName: String = TODO("parse the url")
+        }
+
+    /**
+     * Represents a 404 error.
+     */
     object NotFoundError: AO3Error()
 }
--- src/main/kotlin/service/models/AO3Response.kt
+++ src/main/kotlin/service/models/AO3Response.kt
@@ -1,7 +1,20 @@
 package service.models
 
 sealed class AO3Response<out T> {
+    /**
+     * Represents a success scenario. The expected result of the call is represented by [value].
+     */
     data class Success<out T>(val value: T): AO3Response<T>()
+
+    /**
+     * Represents a scenario where AO3 returns a response that does not indicate success (i.e. not a 2XX response).
+     * The meaning of the [error] is context-specific; further type-checking should be done in order to
+     * discern its meaning.
+     */
     data class ServerError(val error: AO3Error): AO3Response<Nothing>()
+
+    /**
+     * Represents a scenario where the network is encountering connectivity issues.
+     */
     object NetworkError: AO3Response<Nothing>()
 }
