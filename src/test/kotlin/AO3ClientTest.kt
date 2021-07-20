7858c7b00ebb9ce76aa2a0c0e756253328e8740f
Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2021-07-20 21:29:43 +0800

    Set up network test extension and add tests for work-fetching process

--- src/test/kotlin/AO3ClientTest.kt
+++ src/test/kotlin/AO3ClientTest.kt
@@ -1,28 +1,27 @@
+import extensions.AO3ClientParameterResolver
 import kotlinx.coroutines.runBlocking
-import okhttp3.OkHttpClient
-import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtendWith
 import service.models.AO3Response
 import service.requests.WorkRequest
 
-internal class AO3ClientTest {
-    val okHttpClient = OkHttpClient()
-    val ao3Client = AO3Client(okHttpClient)
-
-
+@ExtendWith(AO3ClientParameterResolver::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class AO3ClientTest(private val client: AO3Client) {
     @Test
     fun `Returns a response with the suspending execute method`() {
         val workRequest = WorkRequest.from(2094438)
-        val response = runBlocking { ao3Client.execute(workRequest) }
+        val response = runBlocking { client.execute(workRequest) }
         assertFalse(response is AO3Response.NetworkError)
     }
 
     @Test
     fun `Returns a response with the blocking execute method`() {
         val workRequest = WorkRequest.from(2094438)
-        val response = ao3Client.executeBlocking(workRequest)
+        val response = client.executeBlocking(workRequest)
         assertFalse(response is AO3Response.NetworkError)
     }
 }
